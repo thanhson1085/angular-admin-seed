ec8398d9223f9468521477cf39eed4121b9f7957
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-08-11 15:54:06 +0700

    bug directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,38 +33,6 @@ angular
                     'scripts/directives/sidebar/sidebar.js',
                     'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
                     ]
-                }),
-                $ocLazyLoad.load(
-                {
-                   name:'toggle-switch',
-                   files:['bower_components/angular-toggle-switch/angular-toggle-switch.min.js',
-                          'bower_components/angular-toggle-switch/angular-toggle-switch.css'
-                      ]
-                }),
-                $ocLazyLoad.load(
-                {
-                  name:'ngAnimate',
-                  files:['bower_components/angular-animate/angular-animate.js']
-                }),
-                $ocLazyLoad.load(
-                {
-                  name:'ngCookies',
-                  files:['bower_components/angular-cookies/angular-cookies.js']
-                }),
-                $ocLazyLoad.load(
-                {
-                  name:'ngResource',
-                  files:['bower_components/angular-resource/angular-resource.js']
-                }),
-                $ocLazyLoad.load(
-                {
-                  name:'ngSanitize',
-                  files:['bower_components/angular-sanitize/angular-sanitize.js']
-                }),
-                $ocLazyLoad.load(
-                {
-                  name:'ngTouch',
-                  files:['bower_components/angular-touch/angular-touch.js']
                 });
             }
         }
