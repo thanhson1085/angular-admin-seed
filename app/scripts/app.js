17b32959a8f07535df2debc24064a8ae309730ff
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-08-13 09:57:56 +0700

    reformat indent

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,90 +8,90 @@
  * Main module of the application.
  */
 angular
-  .module('sbAdminApp')
-  .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
-    
+.module('sbAdminApp')
+.config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
+
     $ocLazyLoadProvider.config({
-      debug:false,
-      events:true,
+        debug:false,
+        events:true,
     });
 
     $urlRouterProvider.otherwise('/dashboard/home');
 
     $stateProvider
-      .state('dashboard', {
+    .state('dashboard', {
         url:'/dashboard',
         templateUrl: 'views/dashboard/main.html',
         resolve: {
             loadMyDirectives:function($ocLazyLoad){
                 return $ocLazyLoad.load(
-                {
+                    {
+                        name:'sbAdminApp',
+                        files:[
+                            'scripts/directives/header/header.js',
+                            'scripts/directives/header/header-notification/header-notification.js',
+                            'scripts/directives/sidebar/sidebar.js',
+                            'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                        ]
+                    });
+            }
+        }
+    })
+    .state('dashboard.home',{
+        url:'/home',
+        templateUrl:'views/dashboard/home.html',
+        resolve: {
+            loadMyFiles:function($ocLazyLoad) {
+                return $ocLazyLoad.load({
                     name:'sbAdminApp',
                     files:[
-                    'scripts/directives/header/header.js',
-                    'scripts/directives/header/header-notification/header-notification.js',
-                    'scripts/directives/sidebar/sidebar.js',
-                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                        'scripts/directives/timeline/timeline.js',
+                        'scripts/directives/notifications/notifications.js',
+                        'scripts/directives/chat/chat.js',
+                        'scripts/directives/dashboard/stats/stats.js'
                     ]
                 });
             }
         }
     })
-      .state('dashboard.home',{
-        url:'/home',
-        templateUrl:'views/dashboard/home.html',
-        resolve: {
-          loadMyFiles:function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name:'sbAdminApp',
-              files:[
-              'scripts/directives/timeline/timeline.js',
-              'scripts/directives/notifications/notifications.js',
-              'scripts/directives/chat/chat.js',
-              'scripts/directives/dashboard/stats/stats.js'
-              ]
-            });
-          }
-        }
-      })
-      .state('dashboard.users',{
+    .state('dashboard.users',{
         templateUrl:'views/users.html',
         controller:'UserCtrl',
         url:'/users/:page/:limit',
         resolve: {
-          loadMyFiles:function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name:'sbAdminApp',
-              files:[
-              'scripts/controllers/users.js',
-              'scripts/services/users.js',
-              ]
-            });
-          }
+            loadMyFiles:function($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name:'sbAdminApp',
+                    files:[
+                        'scripts/controllers/users.js',
+                        'scripts/services/users.js',
+                    ]
+                });
+            }
         }
     })
-      .state('dashboard.panels-wells',{
-          templateUrl:'views/ui-elements/panels-wells.html',
-          url:'/panels-wells'
-      })
-      .state('dashboard.buttons',{
+    .state('dashboard.panels-wells',{
+        templateUrl:'views/ui-elements/panels-wells.html',
+        url:'/panels-wells'
+    })
+    .state('dashboard.buttons',{
         templateUrl:'views/ui-elements/buttons.html',
         url:'/buttons'
     })
-      .state('dashboard.notifications',{
+    .state('dashboard.notifications',{
         templateUrl:'views/ui-elements/notifications.html',
         url:'/notifications'
     })
-      .state('dashboard.typography',{
-       templateUrl:'views/ui-elements/typography.html',
-       url:'/typography'
-   })
-      .state('dashboard.icons',{
-       templateUrl:'views/ui-elements/icons.html',
-       url:'/icons'
-   })
-      .state('dashboard.grid',{
-       templateUrl:'views/ui-elements/grid.html',
-       url:'/grid'
-   });
-  }]);
+    .state('dashboard.typography',{
+        templateUrl:'views/ui-elements/typography.html',
+        url:'/typography'
+    })
+    .state('dashboard.icons',{
+        templateUrl:'views/ui-elements/icons.html',
+        url:'/icons'
+    })
+    .state('dashboard.grid',{
+        templateUrl:'views/ui-elements/grid.html',
+        url:'/grid'
+    });
+}]);
--- app/scripts/directives/sidebar/sidebar.js
+++ app/scripts/directives/sidebar/sidebar.js
@@ -8,33 +8,33 @@
  */
 
 angular.module('sbAdminApp')
-  .directive('sidebar',['$location',function() {
+.directive('sidebar',['$location',function() {
     return {
-      templateUrl:'scripts/directives/sidebar/sidebar.html',
-      restrict: 'E',
-      replace: true,
-      scope: {
-      },
-      controller:function($scope){
-        $scope.selectedMenu = 'dashboard';
-        $scope.collapseVar = 0;
-        $scope.multiCollapseVar = 0;
-        
-        $scope.check = function(x){
-          
-          if(x==$scope.collapseVar)
+        templateUrl:'scripts/directives/sidebar/sidebar.html',
+        restrict: 'E',
+        replace: true,
+        scope: {
+        },
+        controller:function($scope){
+            $scope.selectedMenu = 'dashboard';
             $scope.collapseVar = 0;
-          else
-            $scope.collapseVar = x;
-        };
-        
-        $scope.multiCheck = function(y){
-          
-          if(y==$scope.multiCollapseVar)
             $scope.multiCollapseVar = 0;
-          else
-            $scope.multiCollapseVar = y;
-        };
-      }
+
+            $scope.check = function(x){
+
+                if(x==$scope.collapseVar)
+                    $scope.collapseVar = 0;
+                else
+                    $scope.collapseVar = x;
+            };
+
+            $scope.multiCheck = function(y){
+
+                if(y==$scope.multiCollapseVar)
+                    $scope.multiCollapseVar = 0;
+                else
+                    $scope.multiCollapseVar = y;
+            };
+        }
     }
-  }]);
+}]);
