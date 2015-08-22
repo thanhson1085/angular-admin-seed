6d121f44670617157398936e7bc0a9d98770d978
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-08-22 21:47:39 +0700

    change routes

--- server/apis/users.js
+++ server/apis/users.js
@@ -5,36 +5,36 @@ var express = require('express'),
     db = require('../models');
 
 // list users
-router.get('/api/v1/users', function(req, res){
+router.get('/', function(req, res){
     db.User.findAll({
         include: []
     }).then(function(users) {
-        res.send(JSON.stringify({}));
+        res.send(JSON.stringify(users));
     });
 });
 
 // new user
-router.post('/api/v1/users', function(req, res){
+router.post('/', function(req, res){
     res.send(JSON.stringify({}));
 });
 
 // delete user
-router.delete('/api/v1/users/:id', function(req, res){
+router.delete('/:id', function(req, res){
     res.send(JSON.stringify({}));
 });
 
 // user detail
-router.get('/api/v1/users/:id', function(req, res){
+router.get('/:id', function(req, res){
     res.send(JSON.stringify({}));
 });
 
 // update user
-router.put('/api/v1/users/:id', function(req, res){
+router.put('/:id', function(req, res){
     res.send(JSON.stringify({}));
 });
 
 // login
-router.post('/api/v1/login', function(req, res){
+router.post('/login', function(req, res){
     crypto.randomBytes(64, function(ex, buf) {
         var token = buf.toString('base64');
         res.send(JSON.stringify({
