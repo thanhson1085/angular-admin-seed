0f2e00e126b391ef49a0f4cb4242b6bfd5bb1a3b
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-08-24 10:37:14 +0700

    add swagger doccument

--- server/server.js
+++ server/server.js
@@ -3,7 +3,21 @@
 var config = require('config');
 var express = require('express');
 var db = require('./models');
+var fs = require('fs');
+var yaml = require('js-yaml');
 var app = express();
+
+// add-on swagger-ui
+app.use('/swagger', express.static('./node_modules/swagger-ui/dist'));
+
+// redirect page
+app.use('/', express.static('./server/docs'));
+
+// api-json swagger
+app.get('/docs', function(req, res){
+    var docs = yaml.safeLoad(fs.readFileSync('./server/docs/swagger.yml', 'utf8'));
+    res.send(JSON.stringify(docs));
+});
 
 app.use(require('./apis'));
 
