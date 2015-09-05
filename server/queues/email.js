631094f177e31bd3d90044fbc7efc66a14e1e47f
Koding <thanhson1085@gmail.com>
2015-09-05 16:12:10 +0000

    email template

--- server/queues/email.js
+++ server/queues/email.js
@@ -5,14 +5,23 @@ var config = require('config');
 var consumer = {};
 var transporter = nodemailer.createTransport(smtpTransport(config.get('mailer')));
 consumer.name = 'email';
-consumer.task = function(job, done){
-    console.log(job.data, done);
-    transporter.sendMail({
-        from: 'thanhson1085@gmail.com',
-        to: 'thanhson1085@gmail.com',
-        subject: 'hello world!',
-        text: 'Authenticated with OAuth2'
 
+var EmailTemplate = require('email-templates').EmailTemplate;
+var path = require('path');
+var templateDir = path.join(__dirname, '../views/emails/', 'welcome');
+var newsletter = new EmailTemplate(templateDir);
+
+consumer.task = function(job, done){
+    newsletter.render({username: job.data.username}, function (err, results) {
+        transporter.sendMail({
+            from: 'thanhson1085@gmail.com',
+            to: 'thanhson1085@gmail.com',
+            subject: 'hello world!',
+            html: results.html
+
+        });
+      // result.html
+      // result.text
     });
 };
 
