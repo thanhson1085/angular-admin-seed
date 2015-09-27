785ef87fe343de25bf7f7707168ff0ce543e0736
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-09-27 10:00:28 +0000

    usermeta options client side

---
+++ app/scripts/services/options.js
@@ -0,0 +1,75 @@
+'use strict';
+angular.module('sbAdminApp').factory('Options', function($http, httpi, $q, APP_CONFIG) {
+    return {
+        list: function(page, limit){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.options.list;
+            var data = {
+                page: page,
+                limit: limit
+            };
+            httpi({
+                method: 'GET',
+                url: url,
+                data: data
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        get: function(id){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.options.get;
+            httpi({
+                method: 'GET',
+                url: url,
+                data: {
+                    id: id
+                }
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        create: function(data){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.options.create;
+            httpi({
+                method: 'POST',
+                url: url,
+                data: data,
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj){
+                        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                    }
+                    return str.join('&');
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        update: function(data){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.options.update;
+            httpi({
+                method: 'PUT',
+                url: url,
+                data: data,
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj){
+                        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                    }
+                    return str.join('&');
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+    };
+});
---
+++ app/scripts/services/usermeta.js
@@ -0,0 +1,75 @@
+'use strict';
+angular.module('sbAdminApp').factory('Usermeta', function($http, httpi, $q, APP_CONFIG) {
+    return {
+        list: function(page, limit){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.usermeta.list;
+            var data = {
+                page: page,
+                limit: limit
+            };
+            httpi({
+                method: 'GET',
+                url: url,
+                data: data
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        get: function(id){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.usermeta.get;
+            httpi({
+                method: 'GET',
+                url: url,
+                data: {
+                    id: id
+                }
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        create: function(data){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.usermeta.create;
+            httpi({
+                method: 'POST',
+                url: url,
+                data: data,
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj){
+                        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                    }
+                    return str.join('&');
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+        update: function(data){
+            var deferred = $q.defer();
+            var url = APP_CONFIG.services.usermeta.update;
+            httpi({
+                method: 'PUT',
+                url: url,
+                data: data,
+                transformRequest: function(obj) {
+                    var str = [];
+                    for(var p in obj){
+                        str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                    }
+                    return str.join('&');
+                },
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                deferred.resolve(data);
+            }).error(deferred.reject);
+            return deferred.promise;
+        },
+    };
+});
