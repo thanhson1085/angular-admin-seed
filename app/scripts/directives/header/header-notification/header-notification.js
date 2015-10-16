102b813f153e835ca638a89cbc34d2a25a78c7ed
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2015-10-16 01:30:51 +0000

    profile page

--- app/scripts/directives/header/header-notification/header-notification.js
+++ app/scripts/directives/header/header-notification/header-notification.js
@@ -9,10 +9,15 @@
 angular.module('sbAdminApp')
 	.directive('headerNotification',function(){
 		return {
-        templateUrl:'scripts/directives/header/header-notification/header-notification.html',
-        restrict: 'E',
-        replace: true,
-    	}
+            templateUrl:'scripts/directives/header/header-notification/header-notification.html',
+            restrict: 'E',
+            replace: true,
+            scope: {
+            },
+            controller:function($scope, $rootScope){
+                $scope.userId = $rootScope.user_info.id;
+            }
+        }
 	});
 
 
