98afed0b5cbfc91083cebd3228995eb1392d07f4
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-03-07 11:25:22 +0000

    bug Options requst

--- server/middlewares/users.js
+++ server/middlewares/users.js
@@ -4,15 +4,18 @@ var _ = require('lodash');
 var config = require('config');
 var logger = require('../utils/logger');
 module.exports = function(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
     var t = req.get('Authorization');
     var unauthorization = config.get('unauthorization');
     if (_.indexOf(unauthorization, req.url) < 0){
         if (t === undefined) {
             logger.info('Access Denied');
             res.status(401).send(JSON.stringify({}));
-            next();
+            return next();
         }
-        t.replace('Bearer ', '');
+        t = t.replace('Bearer ', '');
         db.Token.findOne({
             where: {
                 token: t
